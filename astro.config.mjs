c02e067500e1951f75c761336ac00e8ee42f1d1f
Robert Romulus <robertromulus@Roberts-MacBook-Pro.local>
2024-01-16 15:49:46 -0500

    made site vercel static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-import vercel from "@astrojs/vercel/serverless";
+import vercelStatic from '@astrojs/vercel/static';
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), mdx()],
-  output: 'server',
-  adapter: vercel(),
+   output: 'static',
+  adapter: vercelStatic(),
 });
+
